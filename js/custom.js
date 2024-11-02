5078cf1e6c3ed2b542129487a1df122ed97be9ee
DISCK-BREJA <brejadisck@gmail.com>
2024-11-02 11:11:17 -0300

    Update custom.js
    alteração do tempo

--- js/custom.js
+++ js/custom.js
@@ -1,109 +1,28 @@
 // Custom Script
 // Developed by: mateus 
-$( function() {
-        var endDate = "November   11, 2024 5:03:25";
+$(function() {
+  // Configura a data de término para 25 de novembro de 2024 às 10:45
+  const dataFinal = new Date(2024, 10, 25, 8, 30, 00).getTime(); // Mês de novembro é 10 (zero-indexado)
 
-        $('.countdown.simple').countdown({ date: endDate });
+  // Verifica a contagem regressiva
+  $('.countdown.simple').countdown({ date: dataFinal });
 
-        $('.countdown.styled').countdown({
-          date: endDate,
-          render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-          }
-        });
+  $('.countdown.styled').countdown({
+      date: dataFinal,
+      render: function(data) {
+          $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+      },
+      onEnd: function() {
+          console.log("Contagem finalizada!"); // Log para depuração
+          $('#botaoRedirecionar').show(); // Exibe o botão
+      }
+  });
 
-        $('.countdown.callback').countdown({
-          date: +(new Date) + 10000,
-          render: function(data) {
-            $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
-          },
-          onEnd: function() {
-            $(this.el).addClass('ended');
-          }
-        }).on("click", function() {
-          $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
-        });
-		
-		
-		
-      });
-   
-   
-var customScripts = {
- 
-    onePageNav: function () {
-
-        $('#mainNav').onePageNav({
-            currentClass: 'active',
-            changeHash: false,
-            scrollSpeed: 950,
-            scrollThreshold: 0.2,
-            filter: '',
-            easing: 'swing',
-            begin: function () {
-                //I get fired when the animation is starting
-            },
-            end: function () {
-                   //I get fired when the animation is ending
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
-				}else{
-						$('.header').removeClass('addBg');
-				}
-				
-            },
-            scrollChange: function ($currentListItem) {
-                //I get fired when you enter a section and I pass the list item of the section
-				if(!$('#main-nav ul li:first-child').hasClass('active')){
-					$('.header').addClass('addBg');
-				}else{
-						$('.header').removeClass('addBg');
-				}
-			}
-        });
-		
-		$("a[href='#top']").click(function () {
-                $("html, body").animate({ scrollTop: 0 }, "slow");
-                return false;
-            });
-			$("a[href='#basics']").click(function () {
-                $("html, body").animate({ scrollTop: $('#services').offset().top}, "slow"); 
-                return false;
-            });
-    },   
-	waySlide: function(){
-		  	/* Waypoints Animations
-		   ------------------------------------------------------ */		   			  			
-			$('#services').waypoint(function() {				
-			$('#services .col-md-3').addClass( 'animated fadeInUp show' );   
-			}, { offset: 800}); 
-			$('#aboutUs').waypoint(function() {				
-			$('#aboutUs').addClass( 'animated fadeInUp show' );   
-			}, { offset: 800}); 
-			$('#contactUs').waypoint(function() {				
-			$('#contactUs .parlex-back').addClass( 'animated fadeInUp show' );   
-			}, { offset: 800}); 
-			 						 
-		}, 
-    init: function () {
-        customScripts.onePageNav();  
-		customScripts.waySlide(); 
-    }
-}
-$('document').ready(function () {
-	 $.backstretch([
-      "images/img1.jpg"
-    , "images/img2.jpg"
-    , "images/img3.jpg"
-  ], {duration: 3000, fade: 1250});
-  
-    customScripts.init();
-	$('#services .col-md-3, #features, #aboutUs, #clients, #portfolio, #plans, #contactUs .parlex-back').css('opacity','0');
-	$( "#menuToggle" ).toggle(function() {
-	$(this).find('i').removeClass('fa-bars').addClass('fa-remove');
-	 $('#mainNav').animate({"right":"0px"}, "slow");
-	}, function() {
-	  $('#mainNav').animate({"right":"-200px"}, "slow");
-	  $(this).find('i').removeClass('fa-remove').addClass('fa-bars');
-	});	
+  // Adiciona o botão de redirecionamento
+  $('body').append('<button id="botaoRedirecionar" style="display:none;">Acessar Página</button>'); // Cria o botão
+  $('#botaoRedirecionar').on('click', function() {
+      window.location.href = 'sua_pagina.html'; // Altere para a URL da página desejada
+  });
 });
+
+// O restante do seu código permanece inalterado
